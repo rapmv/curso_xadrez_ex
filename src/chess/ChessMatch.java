1b405b8a35668129f25b5e8218f5f8c301baa111
Raphael Maciel Vilela <raph.mv.cbjr@gmail.com>
2020-08-27 14:58:28 -0300

    Possiveis movimentos da peça

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,6 +26,13 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	//mostrar as posições possiveis
+	public boolean[][] possibleMoves(ChessPosition sourcePosition){
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
