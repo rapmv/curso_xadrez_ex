5b2d5f6c50bc602008f432ce8b9751db94ef6951
Raphael Maciel Vilela <raph.mv.cbjr@gmail.com>
2020-08-31 10:47:25 -0300

    Promoção Peão Final

--- src/application/Program.java
+++ src/application/Program.java
@@ -45,6 +45,12 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/C/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
